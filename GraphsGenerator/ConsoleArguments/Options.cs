2ffa00eae5440341208d57dbdf42e2646b8dca62
Завенягин Максим <maximzombi@yandex.ru>
2021-04-27 22:40:54 +0400

    Добавьте файлы проекта.

---
+++ GraphsGenerator/ConsoleArguments/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace GraphsGenerator
+{
+    public class Options
+    {
+        [Option('v', "VertexCount", Required = true, HelpText = "Vertex count.")]
+        public int VertexCount { get; set; }
+
+        [Option('w', "WriteToFile", Default = false, HelpText = "Print all graphs to file.")]
+        public bool WriteGraphsToFile { get; set; }
+
+        [Option('f', "FileName", Default = "Graphs.txt", HelpText = "File name for writing graphs. Will be user if parameter -w is true.")]
+        public string FileName { get; set; }
+    }
+}
---
+++ GraphsGenerator/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using CommandLine;
+
+namespace GraphsGenerator
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            var consoleArguments = new Options();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => 
+                {
+                    var generator = new BruteForceGenerator();
+                    var result = generator.Generate(o.VertexCount).ToList();
+                    System.Console.WriteLine($"Total graphs count: {result.Count}.");
+
+                    if (o.WriteGraphsToFile)
+                    {
+                        File.WriteAllLines(o.FileName, result);
+                        System.Console.WriteLine($"Graphs was written to \"{o.FileName}\" file.");
+                        return;
+                    }
+
+                    System.Console.WriteLine("Graphs in g6 format: ");
+                    foreach (var g6 in result)
+                    {
+                        System.Console.WriteLine(g6);
+                    };
+                });
+        }
+    }
+}
